1f56df3398d2ba63d4305b8a5598c46bf7af91c2
Darryl Hebbes <darrylhebbes@Darryls-MBP.fritz.box>
2017-01-15 17:32:18 +0100

    Files added
    - Added: MastHead class
    - Added: Logo SVg file, removed SVG path for 'Developer' word
    - Added: index.js for base render App method

--- src/components/MastHead.js
+++ src/components/MastHead.js
@@ -1,1 +1,18 @@
-// MastHead.js
+
+import React, { Component } from 'react';
+
+class MastHead extends Component {
+    render() {
+        return (
+            <header className="masthead clearfix">
+              <div className="clearfix">
+                <div className="sm-col sm-col-2 "><img alt="" className="fit m2" src="../../public/github-developer-logo.svg"/></div>
+                <div className="sm-col sm-col-10 ">
+                </div>
+              </div>     
+            </header>
+        );
+    }
+}
+
+export default MastHead;
