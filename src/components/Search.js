87a956a9f774f07d9d6df48a838d197e119080ff
Darryl Hebbes <darrylhebbes@Darryls-MBP.fritz.box>
2017-01-15 17:37:36 +0100

    Search class
    - Added methods for handling search events
    - Added render emthod with basic search field
    - Added logic to handle 'Enter' keypress
    - Added Search field state

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,1 +1,35 @@
-// Search.js
+import React, { Component } from 'react';
+
+class Search extends Component {
+    state = {
+        searchTerm: ''
+    }
+
+    handleChange = (evt) => {
+        const term = evt.target.value;
+        this.setState({searchTerm: term});
+
+        if (term.length > 2) {
+            // Search
+        }
+    }
+
+    
+    handleKeyUp = (evt) => {
+        const ENTER_KEY_CODE = 13;
+        if(evt.keyCode === 13) this.emitSearch();
+    }
+    
+    render() {
+        return <input
+        type="text"
+        className="search-box mt3 mb3"
+        onChange={this.handleChange}
+        onKeyUp={this.handleKeyUp}
+        placeholder="Type a module name"
+        value={this.state.searchTerm}
+            />
+    }
+}
+
+export default Search;
