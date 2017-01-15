2d7658ebcdcf0d1f1c9cd794db8ce4f94de5c785
Darryl Hebbes <darrylhebbes@Darryls-MBP.fritz.box>
2017-01-15 17:44:42 +0100

    Added Resultcontainer component basics

--- src/components/ResultContainer.js
+++ src/components/ResultContainer.js
@@ -1,1 +1,12 @@
-// ResultContainer.js
+import React, { Component } from 'react';
+import reqwest from 'reqwest';
+
+class ResultContainer extends Component {
+    render() {
+        return (
+            <div>Results</div>
+        );
+    }
+}
+
+export default ResultContainer;
